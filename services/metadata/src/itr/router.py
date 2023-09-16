b362dfba28269eafcc1b7403a546366310a1832f
LMHoang20 <leminhhoang7554@gmail.com>
2023-09-16 09:00:24 +0700

    chore: delete unnecessary stuff in router

--- services/metadata/src/itr/router.py
+++ services/metadata/src/itr/router.py
@@ -10,9 +10,6 @@ from firebase_admin import storage
 
 from google.cloud import storage
 
-from .beit3_model import Beit3Model
-from .dtb_cursor import DatabaseCursor
-
 import numpy as np
 import csv
 import json
@@ -24,46 +21,7 @@ import json
 client = storage.Client()
 bucket = client.bucket('thangtd1')
 
-class Item(BaseModel):
-    query_text: str
-    topk: int
-
-
 router = APIRouter()
-
-vectordb_cursor = None
-vlm_model = None
-
-
-def init_vectordb(**kargs):
-    # Singleton pattern
-    global vectordb_cursor
-    if vectordb_cursor is None:
-        vectordb_cursor = DatabaseCursor(**kargs)
-
-
-def init_model(device: str):
-    # Singleton
-    global vlm_model
-    if vlm_model is None:
-        vlm_model = Beit3Model(device=device)
-
-
-@router.post("/retrieval/image-text")
-async def retrieve(item: Item) -> JSONResponse:
-    try:
-        query_vector = vlm_model.get_embedding(input=item.query_text)
-        search_results = vectordb_cursor.kNN_search(query_vector, item.topk)
-    except Exception:
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content={"message": "Search error"},
-        )
-
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content={"message": "success", "details": search_results},
-    )
 
 @router.get("/{video}/metadata")
 async def get_length(video: str = Path(..., description = "Video id")) -> JSONResponse:
